6e733e6b5f4e447d4ecb1c5754e1b21f602620a2
Joseph Herrera <tony.herrera90@gmail.com>
2020-10-23 17:23:55 -0700

    add car junit test

--- JPACar/src/main/java/com/skilldistillery/jpacrud/entities/Car.java
+++ JPACar/src/main/java/com/skilldistillery/jpacrud/entities/Car.java
@@ -1,5 +1,42 @@
 package com.skilldistillery.jpacrud.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Car {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Car() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Car [id=" + id + ", name=" + name + "]";
+	}
 
 }
---
+++ JPACar/src/test/java/com/skilldistillery/jpacrud/entities/CarTest.java
@@ -0,0 +1,54 @@
+package com.skilldistillery.jpacrud.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Car car;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+
+		emf = Persistence.createEntityManagerFactory("CarPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+
+		em = emf.createEntityManager();
+		car = em.find(Car.class, 1);
+
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		car = null;
+	}
+
+	@Test
+	void test_Car_entity_mapping() {
+		assertNotNull(car);
+		assertEquals("Tesla", car.getName());
+
+	}
+
+}
